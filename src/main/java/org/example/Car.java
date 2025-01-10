2701da52d82c1acc056cf86a13275c39e595f3a6
Chatinun <Chatinun.hobby@gmail.com>
2025-01-10 21:17:09 +0900

    Initial commit

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Car {
+    public Car(){
+
+    }
+
+    public java.lang.String getEngineName() {
+        return engineName;
+    }
+
+    public void setEngineName(java.lang.String engineName) {
+        this.engineName = engineName;
+    }
+
+    private java.lang.String engineName = "Engine101";
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        System.out.println("Hello world!" + myCar.getEngineName());
+    }
+}
