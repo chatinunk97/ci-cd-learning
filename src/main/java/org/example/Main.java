773d92dac62f5de56c626b77a5a9578a2742faa9
Chatinun <Chatinun.hobby@gmail.com>
2025-01-12 22:12:09 +0900

    change java program to ask for input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Car myCar = new Car();
-        System.out.println("Hello world!" + myCar.getEngineName());
-    }
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("################### ------------------ Java container is online ------------------ ###################");
+		System.out.println("Test Class ---> " + myCar.getEngineName());
+		while (true) {
+			System.out.print("This is working ? Input \"Exit\" to exit: ");
+			String userInput = scanner.nextLine();
+			if ("exit".equalsIgnoreCase(userInput)) {
+				break;
+			}
+		}
+	}
 }
